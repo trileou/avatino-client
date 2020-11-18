4d4c2c5b47ede7705149ce0d7a1ca3f8ad04c7e4
Anh.Tuan <nttanh6299@gmail.com>
2020-11-18 21:57:45 +0700

    Re-layout home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,39 @@
 import * as React from 'react';
+import Slider from 'react-slick';
 
 import Product from '../components/Product';
 
 import hero from '../assets/hero.jpg';
-import Loader from '../components/Loader';
 
 export interface HomeProps {}
+
+const settingsSpecialSlider = {
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+  ]
+};
 
 const Home: React.FC<HomeProps> = () => {
   return (
@@ -19,20 +47,40 @@ const Home: React.FC<HomeProps> = () => {
 
       <div className="container">
         <div className="home_content">
-          <h2 className="list-product-title">SẢN PHẨM MỚI</h2>
-          <div className="list-product">
-            {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, index) => (
-              <Product key={index} />
-            ))}
+          <div className="content-section">
+            <h2 className="products-title">SẢN PHẨM MỚI</h2>
+            <div className="new-products">
+              {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, index) => (
+                <Product key={index} />
+              ))}
+            </div>
+            </div>
+
+          <div className="content-section">
+            <div className="newsletter">
+              <div className="content">
+                <h2>Newsletter</h2>
+                <p>Hãy đăng ký để nhận tin về sản phẩm mới nhất</p>
+              </div>
+              <form className="form">
+                <input type="text" />
+                <button type="submit">Đăng ký</button>
+              </form>
+            </div>
           </div>
 
-          <div className="view-more">
-            <span>
-              <Loader loading={true} />
-            </span>
+          <div className="content-section">
+            <h2 className="products-title">SẢN PHẨM HOT</h2>
+            <div className="special-products">
+              <Slider {...settingsSpecialSlider}>
+                {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, index) => (
+                  <Product key={index} />
+                ))}
+              </Slider>
+            </div>
+          </div>     
           </div>
         </div>
-      </div>
     </div>
   );
 };
